960a3f8a8dbeb3f2e650ff7e1dd09c25e2a80550
Josema <josema.saborido@gmail.com>
2021-11-04 21:15:05 +0100

    Cors añadido

--- src/main/java/com/equipo2/BestJobNew/Security/WebSecurityConfig.java
+++ src/main/java/com/equipo2/BestJobNew/Security/WebSecurityConfig.java
@@ -12,6 +12,10 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 /**
  * Clase encargada de la configuracion de la seguridad de la aplicacion
@@ -30,7 +34,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http.cors().and().authorizeRequests()
                 //Solo se premitiran acceso a los usuarios que tengan los roles de ADMIN y USER a las siguientes endpoints
                 .antMatchers("/swagger-ui").hasRole("USER")
                 .antMatchers("/api/v1/ofertas").hasRole("ADMIN")
@@ -71,4 +75,12 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public UserDetailsService userDetailsServiceBean() throws Exception {
         return super.userDetailsServiceBean();
     }
+
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", new CorsConfiguration().applyPermitDefaultValues());
+        return source;
+    }
+
 }
